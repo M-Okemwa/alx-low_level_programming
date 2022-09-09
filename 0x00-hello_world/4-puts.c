9ff4aedf12137355d29437f3d21b6038b15e0613
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-09 06:28:30 -0700

    Hello, puts)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main-printOut
+ * Return:Always 0 (acceptable)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
+ 
