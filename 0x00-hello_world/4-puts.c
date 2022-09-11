f93942de574ebc868176e910ee91944905e1ea85
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-11 14:24:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main-printing
- *
- * Return: Always 0 (done)
+ *main-entry point
+ *Return:0
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
