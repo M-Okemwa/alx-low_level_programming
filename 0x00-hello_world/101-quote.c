5a7b91df1856200c670f976b37537ff16c5a6f73
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-12 15:36:57 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
-#include <unistd.h>
+
 /**
- * main-prints the standard error sandwiched around the standard output
- * Returns:1
+ * main-printout
+ * Returns: Always 1 (complete)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
