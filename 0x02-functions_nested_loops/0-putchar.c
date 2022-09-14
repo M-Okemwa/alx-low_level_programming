ebf843c8cc329d338b133117f987f48b19d36f4a
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-14 10:57:17 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-printing
+ * prints _putchar
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
