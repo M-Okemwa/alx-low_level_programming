3fc1a7a16f2b8e6965cbcfe7b04ea582bdd58aac
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-19 13:29:48 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integer values
+ * @i: first integer
+ * @j: second integer
+ * Return; Nothing
+ */
+
+void swap_int(int *i, int *j);
+{
+	int tmp =*i;
+
+	*i = *b;
+	*b = tmp;
+
+}
