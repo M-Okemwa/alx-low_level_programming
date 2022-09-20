00d19930fa49768a3bb5c3fd182f5d5a5012cb64
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-20 11:54:27 -0700

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,8 @@
 
 /**
  * rev_string - prints given string in reverse
- * @s: passed pointer argument for string 
+ * @s: passed pointer argument for string
+ *
  * Return: void
  */
 void rev_string(char *s)
@@ -10,13 +11,13 @@ void rev_string(char *s)
 	int len = 0, i, length;
 	char c;
 
-	while (*(s + len) !='\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
 	}
 	length = len;
 	len--;
-	for (i=0; i < length / 2; i++)
+	for (i = 0; i < length / 2; i++)
 	{
 		c = *(s + i);
 		*(s + i) = *(s + len);
