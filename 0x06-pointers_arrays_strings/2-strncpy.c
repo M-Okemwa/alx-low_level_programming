7e260e47562efac2b71fd3d2e85fcc055a25aacc
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-22 05:59:41 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings but add inputted number of bytes
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest
- * @n: The number of bytes from src to be appended to dest
- *
- * Return: A pointer to the resulting string dest
+ * _strncpy - C function that copies a string, including
+ * the terminatng null byte, using at most an inputted number of bytes.
+ * If the length of the source string is less than the maximum byte number,
+ * the remainder of the destination string is filled with null bytes.
+ * works identically to the standard library function strncpy
+ *@dest: buffer storing the string copy
+ *@src: the source string
+ *@n: max number of byte copied
+ *Return: returns
  */
-char *_strncat(char *dest, char *src, int n)
+
+char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, dest_len =0;
+	int i;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
