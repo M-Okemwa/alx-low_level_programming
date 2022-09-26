3e688e33d0c91347623eb60aee41e5a9931ee0b6
M-Okemwa <okemwamjudith21@gmail.com>
2022-09-26 13:08:46 -0700

    fills memory with a constant byte

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- *string_toupper - Changes all lowercase letters
+ *Description: 'lowercase to uppercase'
+
+ * char *string_toupper(char *): prototype
+ * string_toupper - Changes all lowercase letters
  * of a string to uppercase.
  * @str: The string to be changes.
  *
@@ -10,13 +13,16 @@
 char *string_toupper(char *)
 {
 	int index = 0;
+	char str[100];
 
-	while (str[index])
+	while(str[index] != '\0' )
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
-
+		{
+			str[index] = str[index] - 32;
+		}
+		index++;
 	}
 
-	Return (str);
+	Return(str);
 }
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,13 @@
+Even more pointers, arrays and strings
+
+ A function that fills memory with a constant byte.
+ A function that copies memory area.
+ A function that locates a character in a string.
+ A function that searches a string for any of a set of bytes.
+ A function that locates a substring.
+ A function that prints the chessboard.
+ A function that prints the sum of the two diagonals of a square matrix of integers.
+ A function that sets the value of a pointer to a char.
+ A file that contains the password for the crackme2 executable.
+
+
