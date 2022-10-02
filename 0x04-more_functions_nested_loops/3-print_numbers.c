21c82e38a1f11de70c82c655c76183e4ee52d4af
M-Okemwa <okemwamjudith21@gmail.com>
2022-10-02 08:54:05 -0700

    prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,12 @@
  * _putchar: print character
  * Return: 0-9 followed by newline
  */
+int _putchar(int)
 void print_numbers(void)
-void _putchar()
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
 
-	_putchar('\n');
+		_putchar(i ,'\n');
 }
