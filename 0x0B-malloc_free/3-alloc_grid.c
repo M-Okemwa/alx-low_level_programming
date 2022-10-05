98694eed346b2e2be2d2094e81fd1bb5436e271f
M-Okemwa <okemwamjudith21@gmail.com>
2022-10-05 01:42:59 -0700

    a function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,44 @@
 #include "main.h"
 
 /**
- * Description - return pointer to 2d array
- * alloc_grid - to return a pointer
- * str_concat - a function that concatenates two strings.
- * @s1: First string
- * @s2: Second string
+ * alloc_grid - return a pointer to a two
+ * dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
  *
- * Return: NULL in case of failure, but pointer to new string incase of success
+ * Return: pointer to array
  */
-
-int **alloc_grid(int , int)
+int **alloc_grid(int width, int height)
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	int **array;
+	int i = 0, j;
 
 	{
-	if (s1 == NULL)
-		s1 = "";
-	}
-	{
-	if (s2 == NULL)
-		s2 = "";
-	}
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-	{
-	if (concat_str == NULL)
+	if (width == 0 || height == 0)
 		return (NULL);
 	}
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	array = (int **) malloc(sizeof(int *) * height);
+	if (array != NULL)
+	{
+		for ( ; i < height; i++)
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-
-	return (concat_str);
+		{
+			array[i] = (int *) malloc(sizeof(int) * width);
+			if (array[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					array[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}
+	}
+	return (array);
 }
